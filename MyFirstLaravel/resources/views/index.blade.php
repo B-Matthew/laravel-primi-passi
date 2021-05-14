1f7fd23e158fe0008de2cd25700d0633db89a102
Matthew <matsgeco92@hotmail.it>
2021-05-14 15:30:30 +0200

    Esercizio finito

---
+++ MyFirstLaravel/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>laravel</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+  </head>
+  <body>
+    <div class="container">
+      <h1 class="rotate">Ciao Gianluca</h1>
+      <h1 class="rotate">Ciao Olga</h1>
+    </div>
+  </body>
+
+</html>
